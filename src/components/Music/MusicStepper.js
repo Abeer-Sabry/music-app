0314cdc94b6440014ef1633bdf53b48c055a76c2
Abeer-Sabry <abeersabry031@gmail.com>
2022-04-16 00:46:06 +0200

    responsive app

--- src/components/Music/MusicStepper.js
+++ src/components/Music/MusicStepper.js
@@ -6,6 +6,8 @@ import Singers from "../Singers/Singers";
 import Albums from "../Albums/Albums";
 import Songs from "../Songs/Songs";
 import RegisterForm from "../Form/RegisterForm";
+// ---- STYLED-COMPONENTS ---- //
+import { MusicWrapper } from "./MusicStyle";
 // ---- REUSABLE-COMPONENTS ---- //
 import { BackButton, NextButton } from "../../reusableComponents/Button";
 // ---- CONSTANTS ---- //
@@ -85,53 +87,55 @@ const MusicStepper = () => {
   };
   return (
     <CustomContainer style={{ marginBottom: "50px" }}>
-      <Box className="box" sx={{ width: "100%" }}>
-        <Stepper activeStep={activeStep}>
-          {steps.map((label, index) => {
-            const stepProps = {};
-            const labelProps = {};
-            if (isStepOptional(index)) {
-              labelProps.optional = <Typography sx={{ color: "orange" }} variant="caption"></Typography>;
-            }
-            if (isStepSkipped(index)) {
-              stepProps.completed = false;
-            }
-            return (
-              <Step key={label} {...stepProps}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
-              </Step>
-            );
-          })}
-        </Stepper>
-        {activeStep === steps.length ? (
-          <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1 }}>All steps completed - you&apos;re finished</Typography>
-            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-              <Box sx={{ flex: "1 1 auto" }} />
-              <Button onClick={handleReset}>Reset</Button>
-            </Box>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1 }}>{getStepperContent(activeStep)}</Typography>
-            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-              <BackButton color="inherit" disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>
-                Back
-              </BackButton>
-              <Box sx={{ flex: "1 1 auto" }} />
-              {isStepOptional(activeStep) && (
-                <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                  Skip
-                </Button>
-              )}
+      <MusicWrapper>
+        <Box className="box" sx={{ width: "100%" }}>
+          <Stepper activeStep={activeStep}>
+            {steps.map((label, index) => {
+              const stepProps = {};
+              const labelProps = {};
+              if (isStepOptional(index)) {
+                labelProps.optional = <Typography sx={{ color: "orange" }} variant="caption"></Typography>;
+              }
+              if (isStepSkipped(index)) {
+                stepProps.completed = false;
+              }
+              return (
+                <Step key={label} {...stepProps}>
+                  <StepLabel {...labelProps}>{label}</StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
+          {activeStep === steps.length ? (
+            <React.Fragment>
+              <Typography sx={{ mt: 2, mb: 1 }}>All steps completed - you&apos;re finished</Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Box sx={{ flex: "1 1 auto" }} />
+                <Button onClick={handleReset}>Reset</Button>
+              </Box>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Typography sx={{ mt: 2, mb: 1 }}>{getStepperContent(activeStep)}</Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <BackButton color="inherit" disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>
+                  Back
+                </BackButton>
+                <Box sx={{ flex: "1 1 auto" }} />
+                {isStepOptional(activeStep) && (
+                  <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                    Skip
+                  </Button>
+                )}
 
-              <NextButton NextButton sx={{ color: "red" }} disabled={!disabled} onClick={handleNext}>
-                {activeStep === steps.length - 1 ? "Finish" : "Next"}
-              </NextButton>
-            </Box>
-          </React.Fragment>
-        )}
-      </Box>
+                <NextButton NextButton sx={{ color: "red" }} disabled={!disabled} onClick={handleNext}>
+                  {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                </NextButton>
+              </Box>
+            </React.Fragment>
+          )}
+        </Box>
+      </MusicWrapper>
     </CustomContainer>
   );
 };
