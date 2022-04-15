ba1ee7dee5c097b6a6fd6a16cc76fd8ad665d396
Abeer-Sabry <abeersabry031@gmail.com>
2022-04-16 01:22:58 +0200

    responsive music app

--- src/components/Songs/SongsStyle.js
+++ src/components/Songs/SongsStyle.js
@@ -6,4 +6,8 @@ export const SongsWrapper = styled.div`
   grid-column-gap: 20px;
   justify-content: space-between;
   align-items: center;
+  @media only screen and (max-width: 600px) {
+    grid-template-columns: repeat(1, 1fr);
+    margin-bottom: 15px;
+  }
 `;
