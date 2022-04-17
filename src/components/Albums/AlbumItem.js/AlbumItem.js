b0e4869e8a3144a12f9b4899f164a2e8fb3e04ee
Abeer-Sabry <abeersabry031@gmail.com>
2022-04-17 17:52:09 +0200

    Responsive Music App

--- src/components/Albums/AlbumItem.js/AlbumItem.js
+++ src/components/Albums/AlbumItem.js/AlbumItem.js
@@ -6,9 +6,6 @@ import { clickedAlbum } from "../../../Redux/songsSlice/songsSlice";
 import { Wrapper } from "./AlbumItemStyle";
 
 const AlbumItem = ({ id, img, title, songs, selected }) => {
-  // console.log("songsAlbumItem", ...songs);
-  // const hisong = [...songs];
-  // console.log("hitSong", hisong);
   const dispatch = useDispatch();
   // STATE
   const [selectMe, setSelectMe] = useState(selected);
@@ -20,15 +17,11 @@ const AlbumItem = ({ id, img, title, songs, selected }) => {
         onClick={() => {
           setSelectMe(!selectMe);
           dispatch(clickedAlbum(songs));
-          // console.log("dispatched", ...songs);
         }}
       >
         <img src={img} alt="" />
         <p>{title}</p>
       </Wrapper>
-      {/* {hisong.map(item => (
-        <div style={{ color: "white" }}>{item.title}</div>
-      ))} */}
     </>
   );
 };
